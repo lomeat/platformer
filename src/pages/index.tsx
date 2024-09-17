8c22bc8bf7e21a8da64645577b9becee6102923e
lomeat <lom3at@gmail.com>
2024-09-17 19:36:06 +0400

    @lomeat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>Phaser Nextjs Template</title>
+				<title>@lomeat</title>
 				<meta name="description" content="я миша, а ты?" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.png" />
