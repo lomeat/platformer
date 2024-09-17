519fd314a2383f31e10c6bb1f8d220faac8dd48b
lomeat <lom3at@gmail.com>
2024-09-17 19:34:00 +0400

    познакомимся?

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,17 +8,18 @@ const inter = Inter({ subsets: ["latin"] });
 const AppWithoutSSR = dynamic(() => import("@/App"), { ssr: false });
 
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <title>Phaser Nextjs Template</title>
-                <meta name="description" content="A Phaser 3 Next.js project template that demonstrates Next.js with React communication and uses Vite for bundling." />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.png" />
-            </Head>
-            <main className={`${styles.main} ${inter.className}`}>
-                <AppWithoutSSR />
-            </main>
-        </>
-    );
+	return (
+		<>
+			<Head>
+				<title>Phaser Nextjs Template</title>
+				<meta name="description" content="я миша, а ты?" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.png" />
+			</Head>
+			<main className={`${styles.main} ${inter.className}`}>
+				<AppWithoutSSR />
+			</main>
+		</>
+	);
 }
+
